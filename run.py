9ff12321bd2f73c14a1dc811d9ac1526e3ac3735
Github User Name <you@example.com>
2018-11-20 10:35:53 +0300

    created functions to implement the behaviours we just created

--- run.py
+++ run.py
@@ -1,2 +1,34 @@
 #!/usr/bin/env python3.6
 from account import Account 
+
+def create_account(app_account,password):
+    '''
+    Function to create a new account
+    '''
+    new_account = Account(app_account,password)
+    return new_account
+
+def save_account(account):
+    '''
+    Function to save account
+    '''
+    account.save_account()
+
+def del_account(account):
+    '''
+    Function to delete a account
+    '''
+    account.delete_account()
+
+
+def find_account(app_account):
+    '''
+    Function that finds a account by app_account name and returns the account
+    '''
+    return Account.find_by_app_account(app_account)
+
+def check_existing_account(app_account):
+    '''
+    Function that check if a account exists with that account name and return a Boolean
+    '''
+    return Account.account_exist(app_account)
