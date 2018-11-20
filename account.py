d95d1042cf49d6dbdcfef25d511b631db3ff9f89
Github User Name <you@example.com>
2018-11-20 10:08:51 +0300

    innitialized my application

--- account.py
+++ account.py
@@ -5,14 +5,16 @@ class Account:
 
     account_list = [] # Empty contact list
 
-    def __init__(self,app_account,password):
-
-      # docstring removed for simplicity
-
-        self.app_account = app_account
-        self.password = password
-
     
+
+    def save_account(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Account.account_list.append(self)
+
     def delete_account(self):
 
         '''
